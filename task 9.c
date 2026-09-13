050cc60b780a62f03bc826cfdd36acffe4c664af
Kareena Suneel <kareenasuneel11@gmail.com>
2026-09-13 22:06:47 +0500

    Add files via upload

---
+++ task 9.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+	printf("student information:\n");
+	printf("Name:\"your name \"\n");
+	printf("Roll no:\t123\n");
+	printf("section:\tA\n\n");
+	printf("path:c:\\user\\student");
+}
+
